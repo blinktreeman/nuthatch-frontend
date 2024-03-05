04383657a3eab2d44061b9adfbaeefda63b93b54
Eugene Elantsev <blinktreeman@gmail.com>
2024-03-05 22:37:16 +0900

    initial commit

---
+++ src/app/representative-service/create-representative/create-representative.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-representative',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-representative.component.html',
+  styleUrl: './create-representative.component.css'
+})
+export class CreateRepresentativeComponent {
+
+}
---
+++ src/app/representative-service/representative-list/representative-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-representative-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './representative-list.component.html',
+  styleUrl: './representative-list.component.css'
+})
+export class RepresentativeListComponent {
+
+}
